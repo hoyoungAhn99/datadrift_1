adf174fdb7cdfbd819b82406693eb8b5428d5880
hoyoungAhn99 <dksghdud2020@gmail.com>
2026-04-11 10:56:16 +0900

    featgen var change

--- drift_detection/HierOOD/GM-ProHOC/commands/feature_gen_set_vars.ps1
+++ drift_detection/HierOOD/GM-ProHOC/commands/feature_gen_set_vars.ps1
@@ -1,3 +1,3 @@
 $REPOROOT = Split-Path -Parent $PSScriptRoot
 $CONFIG = Join-Path $REPOROOT "configs\experiments\fgvc_aircraft_weihims_clip.yaml"
-$FEATURE_GEN_CONFIG = Join-Path $REPOROOT "configs\feature_gen\identity.yaml"
+$FEATURE_GEN_CONFIG = Join-Path $REPOROOT "configs\feature_gen\variance_mask.yaml"
