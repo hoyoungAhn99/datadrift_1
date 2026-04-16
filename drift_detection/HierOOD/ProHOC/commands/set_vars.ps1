46c865cf327f786cf262438a72063584e7bff954
hoyoungAhn99 <dksghdud2020@gmail.com>
2026-04-16 14:54:53 +0900

    config change

--- drift_detection/HierOOD/ProHOC/commands/set_vars.ps1
+++ drift_detection/HierOOD/ProHOC/commands/set_vars.ps1
@@ -1,5 +1,5 @@
 $PROHOC="C:\HOYOUNG\datadrift_1\drift_detection\HierOOD\ProHOC"
 $PROHOCDATA="D:\HY_Data\HierOOD"
 $TRAINDIR="C:\HOYOUNG\datadrift_1\drift_detection\HierOOD\ProHOC\ckpts"
-$DSET="fgvc-aircraft"
-$HEIGHTS = 0, 1, 2
+$DSET="inat19"
+$HEIGHTS = 0, 1, 2, 3, 4, 5
