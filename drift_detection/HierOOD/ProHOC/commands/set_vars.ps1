5141053d943bb4e5f921429890d5b6aec8ac96f1
hoyoungAhn99 <dksghdud2020@gmail.com>
2026-04-16 15:22:46 +0900

    set_vars updated

--- drift_detection/HierOOD/ProHOC/commands/set_vars.ps1
+++ drift_detection/HierOOD/ProHOC/commands/set_vars.ps1
@@ -1,5 +1,12 @@
 $PROHOC="C:\HOYOUNG\datadrift_1\drift_detection\HierOOD\ProHOC"
-$PROHOCDATA="D:\HY_Data\HierOOD"
+$PROHOCDATA="C:\HOYOUNG\dataset\HierOOD"
 $TRAINDIR="C:\HOYOUNG\datadrift_1\drift_detection\HierOOD\ProHOC\ckpts"
 $DSET="inat19"
 $HEIGHTS = 0, 1, 2, 3, 4, 5
+
+
+$FGVCSOURCE="$PROHOCDATA\FullData\fgvc-aircraft-2013b"
+$IMAGENETSOURCE="$PROHOCDATA\FullData\imagenet-object-localization-challenge\ILSVRC\Data\CLS-LOC"
+$INATSOURCE="$PROHOCDATA\FullData\inaturalist-2019-fgvc6"
+$INATSPLITS="$PROHOC\data\splits_inat19"
+$IMAGENETVALLABELS="$PROHOCDATA\FullDataset\imagenet-object-localization-challenge\LOC_val_solution.csv"
